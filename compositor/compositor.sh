93b6d5e0804207fa055b999ee5154774d4c30d3c
Olivia Artz <liv@oliviaartz.com>
2021-01-09 14:25:46 -0500

    Functioning command line compositor that does one frame at a time

--- compositor/compositor.sh
+++ compositor/compositor.sh
@@ -1,97 +1,128 @@
 #!/usr/bin/env node
 
-const fs = require("fs"),
-			path = require('path')
+const { createCanvas, loadImage } = require('canvas'),
+			fs = require("fs"),
+			path = require('path'),
+			calWidth = 3840,
+			calHeight = 2160,
+			cellWidth = calWidth / 7,
+			cellHeight = calHeight / 6,
+			cellAspect = cellWidth / calHeight,
+			monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"],
+			// can be 'png', 'jpeg', or 'tiff'
+			// 'jpeg' appears to write to storage the fastest, even when there isn't much files savings
+			stillImageOutputFormat = 'jpeg',
+			canvas = createCanvas(calWidth, calHeight),
+			ctx = canvas.getContext('2d'),
+			args = process.argv.slice(2)
 
-var imagesByDay = []
-var patchDir = __dirname.split(path.sep)
+var calMonth,
+		calYear,
+		patchDir = __dirname.split(path.sep)
+
 patchDir.pop()
 patchDir = patchDir.join(path.sep)
-var calWidth = 3840
-var calHeight = 2160
-var cellWidth = calWidth / 7
-var cellHeight = calHeight / 6
-var cellAspect = cellWidth / calHeight
 
-var monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
-var frameNum = 0
-
-// can be 'png', 'jpeg', or 'tiff'
-// 'jpeg' appears to write to disk the fastest, even when there isn't much files savings
-var stillImageOutputFormat = 'jpeg'
-
-var showTime = function (t) {
-	var framesFound = []
+var renderFrame = function (t, month, year) {
 	
-	for ( var i = 0; i < imagesByDay.length; i++ ) {
-		possibleImage = imagesByDay[i][t]
-		if (possibleImage) {
-			framesFound.push(possibleImage)
+	// clear the canvas to white
+	ctx.beginPath()
+	ctx.clearRect(0, 0, canvas.width, canvas.height)
+	ctx.fillStyle = '#fff'
+	ctx.fillRect(0, 0, canvas.width, canvas.height)
+
+	// - what month are we displaying? if unspecified, it’s this month
+	if (typeof month === 'undefined' || typeof year === 'undefined') {
+		var now = new Date()
+	}
+	if (typeof month === 'undefined') {
+		month = now.getMonth()
+	}
+	if (typeof year === 'undefined') {
+		year = now.getFullYear()
+	}
+	calMonth = parseInt(month)
+	calYear = parseInt(year)
+
+	var monthName = monthNames[month]
+
+	// - write month, year, & time being shown
+	drawTime(t)
+
+	// - how many days in this month? for loop for each day in the month…
+	var numDays = daysInMonth(month, year)
+
+	// setting up the font for the day numbers
+	ctx.font = '54px Helvetica Neue Light'
+
+	var imagePromises = []
+
+	for (var i = 0; i < numDays; i++) {
+
+		// - determine the ltrb for this calendar cell
+		const cellLtrb = dayToCalPos(i)
+		const dayNum = i
+
+		// - is there a file for that time in the folder for this day?
+		const day = String(i+1),
+					pathStr = `../images/${month+1}-${day}-${year}/${month+1}-${day.padStart(2, 0)}-${year}-${String(t).padStart(2, 0)}.png`
+
+		if (fs.existsSync(pathStr)) {
+			// - if found, draw image w correct srcrect at lrtb then write date in white
+			var thisPromise = loadImage(pathStr)
+			imagePromises.push(thisPromise)
+
+			thisPromise.then((image) => {
+
+				var sourceAspect = image.width / image.height
+				// TODO: Deal with other case, where calendar cells are actually wider than the source image
+				if (sourceAspect > cellAspect) {
+					var hRatio = image.height / cellHeight
+					var scaledSourceWidth = image.width / hRatio
+					var sourceXOffset = (scaledSourceWidth - cellWidth) * hRatio / 2
+					var sourceWidth = image.width - (sourceXOffset * 2)
+				}
+				ctx.drawImage(image, sourceXOffset, 0, sourceWidth, image.height, Math.floor(cellLtrb[0]), cellLtrb[1], cellWidth, cellHeight)
+				// set context to write the day number in white on top of the frame we just drew
+				ctx.fillStyle = '#fff'
+				ctx.fillText(dayNum + 1, cellLtrb[0] + 16, cellLtrb[1] + 54)			
+			})
+
+		}
+		else {
+			// - set the context to write the day number in black
+			ctx.fillStyle = '#000'
+			ctx.fillText(dayNum + 1, cellLtrb[0] + 16, cellLtrb[1] + 54)
 		}
 	}
-	frameNum = t
-	outlet(1, 'clear')
-	drawText()
-	drawFrames(framesFound)
-	outlet(1, 'bang')
+
+	Promise
+	.all(imagePromises)
+	.then(function() {
+		const out = fs.createWriteStream(`../export/${t}.png`)
+		const stream = canvas.createPNGStream()
+		stream.pipe(out)	
+	})
 }
 
-var exportTime = function (t) {
-	showTime(t)
-	outlet(0, 'exportimage', 'export/' + t, stillImageOutputFormat)
-}
+var drawTime = function(time) {
+	// draw text in black
+	ctx.fillStyle = '#000'
 
-var drawText = function(time) {
-	var d = new Date()
-	var month = monthNames[d.getMonth()]
-	
-	// jit.lcd method below
-	outlet(1, 'frgb', 0, 0, 0)
-	outlet(1, 'moveto', 80, 240)
-	outlet(1, 'font', 'Helvetica Neue', 200)
-	outlet(1, 'textface', 'bold')
-	outlet(1, 'write', month)
-	outlet(1, 'textface', 'normal')
-	outlet(1, 'write', d.getFullYear())
-	outlet(1, 'font', 'Helvetica', 200)
-	outlet(1, 'font', 'Helvetica Neue Ultralight', 200)
-	outlet(1, 'moveto', cellWidth * 5, 240)
-	outlet(1, 'write', frameNumToTime(frameNum))
+	// draw name of month
+	ctx.font = 'bold 200px Helvetica Neue'
+	ctx.fillText(monthNames[calMonth], 80, 240)
 
-	outlet(1, 'frgb', 0, 0, 0)
-	outlet(1, 'font', 'Helvetica Neue Light', 54)
-	var numDays = daysInThisMonth()
-	for ( var day = 0; day < numDays; day++ ) {
-		var calCoords = dayToCalCoords(day)
-		outlet(1, 'moveto', (calCoords[0] * cellWidth) + 16, (calCoords[1] * cellHeight) + 54)
-		outlet(1, 'write', day + 1)
-	}
+	// measure name of month to the position the text for the year
+	var monthWidth = ctx.measureText(monthNames[calMonth] + '   ').width // spaces added to give them breathe
 
-}
+	// draw the year
+	ctx.font = '200px Helvetica Neue'
+	ctx.fillText(calYear, monthWidth, 240)
 
-var drawFrames = function (frames) {
-	for ( var i = 0; i < frames.length; i++ ) {
-		var destLtrb = dayToCalPos(frames[i].day)
-		var img = new Image(frames[i].path)
-
-		var sourceAspect = img.size[1] / img.size[0]
-		// TODO: Deal with other case, where calendar cells are actually wider than the source image
-		if (sourceAspect > cellAspect) {
-			var hRatio = img.size[1] / cellHeight
-			var scaledSourceWidth = img.size[0] / hRatio
-			var sourceXOffset = (scaledSourceWidth - cellWidth) * hRatio / 2
-			var sourceWidth = img.size[0] - (sourceXOffset * 2)
-		}
-
-		outlet(1, 'readpict', 'pictname' + i, frames[i].path)
-		outlet(1, 'drawpict', 'pictname' + i, Math.floor(destLtrb[0]), destLtrb[1], Math.ceil(destLtrb[2] - destLtrb[0]), destLtrb[3] - destLtrb[1], sourceXOffset, 0, sourceWidth, img.size[1])
-		outlet(1, 'frgb', 255, 255, 255)
-		var calCoords = dayToCalCoords(frames[i].day)
-		outlet(1, 'moveto', (calCoords[0] * cellWidth) + 16, (calCoords[1] * cellHeight) + 54)
-		outlet(1, 'write', frames[i].day + 1)
-
-
-	}
+	// draw the normal AM/PM time
+	ctx.font = 'light 200px Helvetica Neue'
+	ctx.fillText(frameNumToTime(time), cellWidth * 5, 240)
 }
 
 var dayToCalPos = function(dayNum) {
@@ -101,7 +132,7 @@ var dayToCalPos = function(dayNum) {
 }
 
 var dayToCalCoords = function(dayNum) {
-	var d = new Date()
+	var d = new Date(calYear, calMonth)
 	d.setDate(1)
 	var dayOffset = d.getDay()
 	var xPos = (dayNum + dayOffset) % 7
@@ -109,61 +140,8 @@ var dayToCalCoords = function(dayNum) {
 	return [xPos, yPos]
 }
 
-var loadImages = function () {
-	var now = new Date()
-	var year = now.getFullYear()
-	var month = now.getMonth()+1
-	var numDays = daysInThisMonth()
-
-	for ( var i = 0; i < numDays; i++ ) {
-		var dayPath = patchDir + '/images/' + month + '-' + (i+1) + '-' + year
-		// console.log(dayPath)
-		fs.readdir(dayPath, function (err, files) {
-			if (err) {
-				// console.error("Could not list the directory.", err);
-			}
-			else {
-				var todaysImages = []
-				files.forEach(function (file) {
-					// console.log(dayPath, file)
-					var nameSplit = file.split('-')
-					var fileMonth = parseInt(nameSplit[0])
-					var fileDay = parseInt(nameSplit[1])
-					var fileYear = parseInt(nameSplit[2])
-					var imageNum = parseInt(nameSplit[3].split('.')[0])
-					todaysImages[imageNum] = {
-						'path': patchDir + "/images/" + fileMonth + "-" + fileDay + "-" + fileYear + "/" + file,
-						'filename': file,
-						'time': imageNum,
-						'day': fileDay
-					}
-					// console.log(fileDay, todaysImages)
-					// console.log(imagesByDay, fileDay, todaysImages)
-
-				})
-				console.log(todaysImages)
-				// imagesByDay[i] = todaysImages
-
-				// console.log(imagesByDay)
-
-			}
-
-	
-		})
-
-	}
-	console.log(imagesByDay)
-}
-
-function daysInMonth (month, year) {
-	return new Date(year, month, 0).getDate();
-}
-
-var daysInThisMonth = function() {
-	var now = new Date()
-	var year = now.getFullYear()
-	var month = now.getMonth()+1
-	return daysInMonth(month, year)
+var daysInMonth = function(month, year) {
+	return new Date(year, month + 1, 0).getDate()
 }
 
 var frameNumToTime = function(f) {
@@ -196,5 +174,4 @@ var makeDirectory = function(dir) {
 
 makeDirectory('../export')
 
-loadImages()
-console.log(imagesByDay)
+renderFrame(args[0], args[1], args[2])
